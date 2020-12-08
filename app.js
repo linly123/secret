17e93e40aa4813c78852f6e20fbdc511873fac84
chloechloe <chloeying84@gmail.com>
2020-12-08 23:06:39 +0800

    Use md5 to encrypy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 
 
 const app = express();
@@ -19,8 +20,6 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema)
@@ -40,7 +39,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const NewUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
 
   NewUser.save(function(err){
@@ -54,7 +53,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const userName = req.body.username
-  const password = req.body.password
+  const password = md5(req.body.password)
   
   User.findOne({email: userName}, function(err, result){
     if(err){
